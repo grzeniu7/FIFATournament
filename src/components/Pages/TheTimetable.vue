97b62a9734f7c34e31889fa9652c6640e812f14e
grzeniu7 <grzegorzkpawlowski@gmail.com>
2021-05-10 00:54:25 +0200

    Added timetable as form for each game

--- src/components/Pages/TheTimetable.vue
+++ src/components/Pages/TheTimetable.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
     <!-- <p>{{ players }}</p> -->
-    <div :key="round" v-for="(round, index) in tournamentPairs">
-      <p>{{ index }}</p>
-      <div :key="pair" v-for="pair in tournamentPairs[index]">
-        <p>{{ pair.home.team }} - {{ pair.away.team }}</p>
-      </div>
-    </div>
+    <ul
+      :key="round"
+      v-for="(round, index) in tournamentPairs"
+      class="timetable"
+    >
+      <p>Runda {{ index + 1 }}</p>
+      <li
+        :key="pair"
+        v-for="pair in tournamentPairs[index]"
+        class="timetable__item"
+      >
+        <form class="form" @submit.prevent="clicked">
+          <div class="form__container">
+            <div class="form__item form__item-reversed">
+              <label for="home" class="form__team">{{ pair.home.team }}</label>
+              <input type="number" placeholder="Score" class="form__goals" />
+            </div>
+            <span>-</span>
+            <div class="form__item">
+              <label for="away" class="form__team form__team-reversed">
+                {{ pair.away.team }}</label
+              >
+              <input type="number" placeholder="Score" class="form__goals" />
+            </div>
+          </div>
+          <base-button type="submit">Confirm result</base-button>
+        </form>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -23,6 +46,10 @@ export default {
     },
   },
   methods: {
+    clicked(event) {
+      console.log(event);
+    },
+
     // showPlayers() {
     //   console.log(this.playersList);
     // },
@@ -68,3 +95,57 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.timetable {
+  padding: 1rem;
+  background-color: rgba(0, 0, 0, 0.7);
+  &__item {
+    list-style: none;
+  }
+}
+
+.form {
+  // background-color: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  //   // width: 100%;
+  &__container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    margin-top: 0.5rem;
+    // background-color: pink;
+    padding: 0.5rem;
+    border: 0.1rem solid white;
+  }
+  &__item {
+    display: flex;
+    // justify-content: center;
+    // align-items: center;
+    flex: 1 50%;
+    padding: 0.5rem 0.5rem;
+    &-reversed {
+      justify-content: flex-end;
+    }
+  }
+  &__team {
+    // flex: ;
+    // background-color: blue;
+    &-reversed {
+      order: 1;
+    }
+  }
+  &__goals {
+    // max-width: 5rem;
+    padding: 0;
+    margin: 0 0.2rem;
+    width: 3.5rem;
+    text-align: center;
+    // flex: 0 1 auto;
+  }
+}
+</style>
